76cf143984e8e0aff56a4de20fb1e8a1dfa34138
manvantara <allo3000@msn.com>
2016-10-04 15:13:09 +0200

    Ajout de la classe compute pour les calcules

---
+++ app/src/main/java/com/kmourad/calculatrice/Compute.java
@@ -0,0 +1,8 @@
+package com.kmourad.calculatrice;
+
+/**
+ * Created by Human Booster on 04/10/2016.
+ */
+
+public class Compute {
+}
--- app/src/main/java/com/kmourad/calculatrice/MainActivity.java
+++ app/src/main/java/com/kmourad/calculatrice/MainActivity.java
@@ -2,12 +2,29 @@ package com.kmourad.calculatrice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static android.R.attr.tag;
 
 public class MainActivity extends AppCompatActivity {
+    static int valueShow = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.EditText01);
+        textView.setText(""+valueShow);
+    }
+
+    public void onButtonClicked(View view){
+        Button button = (Button) view;
+        TextView textView = (TextView) findViewById(R.id.EditText01);
+        textView.setText(button.getTag().toString());
+       // valueShow = Integer.parseInt(textView.getText().toString());
     }
 }
