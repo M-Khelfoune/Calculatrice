a5e4bfb5a540798cdef68dc01008536b75c78858
manvantara <allo3000@msn.com>
2016-10-11 09:38:54 +0200

    Making landscape for calculator

--- app/src/main/java/com/kmourad/calculatrice/MainActivity.java
+++ app/src/main/java/com/kmourad/calculatrice/MainActivity.java
@@ -1,18 +1,38 @@
 package com.kmourad.calculatrice;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static com.kmourad.calculatrice.R.id.fragment_normal;
+
 public class MainActivity extends AppCompatActivity {
-    static Compute compute = new Compute();
+    static protected Compute compute = new Compute();
+    static TextView textView;
+/* For Call normalFragment
+    static NormalFragment normalFragment;
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.EditText01);
+
+/*  How TO Call normalFragment
+        if (savedInstanceState == null) {
+            normalFragment = (NormalFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_normal);
+        }
+        normalFragment.setCompute(compute);
+*/
+    }
+
+    public void setDisplay(String toShow) {
+        textView.setText(toShow);
 
     }
 
--- app/src/main/java/com/kmourad/calculatrice/NormalFragment.java
+++ app/src/main/java/com/kmourad/calculatrice/NormalFragment.java
@@ -3,15 +3,25 @@ package com.kmourad.calculatrice;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
 import static com.kmourad.calculatrice.MainActivity.compute;
 
 public class NormalFragment extends Fragment {
+/* PARAMETER FROM MAINACTIVITY
+    static Compute compute = null;
+*/
+    int[] buttonIDs = new int[]{R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
+            R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9,
+            R.id.buttonCE, R.id.buttonC, R.id.buttonBack, R.id.buttonDivise, R.id.buttonEgale,
+            R.id.buttonMoins, R.id.buttonMultiplie, R.id.buttonPlus, R.id.buttonVirgule};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,42 +31,62 @@ public class NormalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //        TextView textView = (TextView) view.findViewById(R.id.EditText01);
-//        textView.setText(compute.getCurrentValue());
-        return inflater.inflate(R.layout.fragment_normal, container, false);
+        String nameButton = "";
+        View view = inflater.inflate(R.layout.fragment_normal, container, false);
+        for (int i = 0; i < buttonIDs.length; i++) {
+            Button b = (Button) view.findViewById(buttonIDs[i]);
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onButtonClicked(v);
+                }
+            });
+        }
+        return view;
     }
 
-/*
+/* RECEIVING PARAMETER FROM MAINACTIVITY
+    public void setCompute(Compute myCompute) {
+        compute = myCompute;
+    }
+*/
+
     public void onButtonClicked(View view) {
         Button button = (Button) view;
-        TextView textView = (TextView) view.findViewById(R.id.EditText01);
-        compute.onKeyClikedIsValid(button.getTag().toString());
-        textView.setText(compute.getCurrentValue());
+        if (compute == null) {
+            return;
+        }
+        ((MainActivity) getActivity()).compute.onKeyClikedIsValid(button.getTag().toString());
+        ((MainActivity) getActivity()).setDisplay(((MainActivity) getActivity()).compute.getCurrentValue().toString());
     }
 
     private void sortInputButton(String keyCliked) {
         char key = keyCliked.charAt(0);
+        /* FOR CALLING FROM MAINACTIVITY
+        if (compute == null) {
+            return;
+        }
+        */
         if ((key >= '0') && (key <= '9')) {
-            compute.readNumbers(key);
+            ((MainActivity) getActivity()).compute.readNumbers(key);
         }
         if (key == 'C') {
-            compute.setClear();
+            ((MainActivity) getActivity()).compute.setClear();
         }
         if (key == ',') {
-            compute.acceptedComma();
+            ((MainActivity) getActivity()).compute.acceptedComma();
         }
         if ((key == '/') || (key == '*') || (key == '-') || (key == '+')) {
-            compute.setOperation(key);
+            ((MainActivity) getActivity()).compute.setOperation(key);
         }
         if (key == '=') {
-            compute.runOperation();
+            ((MainActivity) getActivity()).compute.runOperation();
         }
         if (key == 'D') {
-            compute.setBackspace();
+            ((MainActivity) getActivity()).compute.setBackspace();
         }
         if (key == 'E') {
-            compute.setClearCurrent();
+            ((MainActivity) getActivity()).compute.setClearCurrent();
         }
     }
-*/
 }
